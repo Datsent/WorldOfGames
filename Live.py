e225fa45d5ab47ea2dfdd598d21ea1370e1766a6
Datsent <datsent@gmail.com>
2022-01-13 19:17:50 +0200

    part II

---
+++ Live.py
@@ -0,0 +1,33 @@
+class bcolors:
+    '''
+    Some colors for outputs
+    '''
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+def welcome(name):
+    return 'Hello ' + bcolors.BOLD + bcolors.OKGREEN + f'{name}' + bcolors.ENDC + ' and welcome to the World of' \
+            ' Games (WoG).\nHere you can find many cool games to play.'
+
+def load_game():
+    games = ['Memory Game', 'Guess Game', 'Currency Roulette']
+    description_game = {
+        'Memory Game': 'a sequence of numbers will appear for 1 second and you have to guess it back',
+        'Guess Game': 'guess a number and see if you chose like the computer',
+        'Currency Roulette': 'try and guess the value of a random amount of USD in ILS'
+    }
+    for game in games:
+       print(f'{(games.index(game) + 1)}. {game} - {description_game[game]}')
+    choosen_game = input()
+    while choosen_game.isnumeric() is False or int(choosen_game) > len(games) or int(choosen_game) <= 0:
+        choosen_game = input('Wrong input, try again: ')
+    choosen_difficulty = input("Please choose game difficulty from 1 to 5: ")
+    while choosen_difficulty.isnumeric() is False or int(choosen_difficulty) > 5 or int(choosen_difficulty) <= 0:
+        choosen_difficulty = input('Wrong input, try again: ')
+    return games[int(choosen_game) - 1], choosen_difficulty
---
+++ MainGame.py
@@ -0,0 +1,60 @@
+from Live import load_game, welcome
+import GuessGame, MemoryGame, CurrencyRouletteGame
+
+class bcolors:
+    '''
+    Some colors for outputs
+    '''
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+def results(user_choose):
+    if user_choose[0] == "Guess Game":
+        result = GuessGame.play(user_choose[1])
+        if result[0]:
+            print("You are " + bcolors.BOLD + bcolors.OKGREEN + "WINNER" + bcolors.ENDC)
+        else:
+            print(bcolors.FAIL + ":( LOOSER ):" + bcolors.ENDC)
+            print(f"The number is {result[1]}")
+    elif user_choose[0] == "Memory Game":
+        result = MemoryGame.play(user_choose[1])
+        if result[0]:
+            print("You are " + bcolors.BOLD + bcolors.OKGREEN + "WINNER" + bcolors.ENDC)
+        else:
+            print(bcolors.FAIL + ":( LOOSER ):" + bcolors.ENDC)
+            print(f"The number is {result[1]}")
+    elif user_choose[0] == "Currency Roulette":
+        result = CurrencyRouletteGame.play(user_choose[1])
+        print(result)
+        if result[0]:
+            print(f"The result is {result[1]} You are " + bcolors.BOLD + bcolors.OKGREEN
+                  + "WINNER" + bcolors.ENDC)
+        else:
+            print(bcolors.FAIL + ":( LOOSER ):" + bcolors.ENDC)
+            print(f"The number is {result[1]}")
+def main():
+    print(welcome(input('Please, enter your name: ')))
+    user_choose = load_game()
+    print('Chosen Game is:', user_choose[0], '\nChosen Difficult is:', user_choose[1])
+    results(user_choose)
+    ask_for_continue = ""
+    while ask_for_continue != '3':
+        ask_for_continue = input('You want:\n1. Play again this game.\n2. Choose another Game.\n3. Exit.')
+        while ask_for_continue.isnumeric() is False or int(ask_for_continue) > 3 or int(ask_for_continue) <= 0:
+            ask_for_continue = input('Wrong input, try again: ')  # Check correct input
+        if ask_for_continue == '1':
+            results(user_choose)
+        elif ask_for_continue == '2':
+            user_choose = load_game()
+            results(user_choose)
+        else:
+            print("Thank you, Goodbay <3 !!!")
+
+if __name__ == '__main__':
+    main()
