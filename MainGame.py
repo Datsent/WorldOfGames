811549dd481d99dc8031fbc4f2a67ead61fb18d1
Datsent <datsent@gmail.com>
2022-01-16 19:16:41 +0200

    add folder

--- MainGame.py
+++ MainGame.py
@@ -8,7 +8,7 @@ class bcolors:
     OKBLUE = '\033[94m'
     OKCYAN = '\033[96m'
     OKGREEN = '\033[92m'
-    WARNING = '\033[93m'cd 
+    WARNING = '\033[93m'
     FAIL = '\033[91m'
     ENDC = '\033[0m'
     BOLD = '\033[1m'
