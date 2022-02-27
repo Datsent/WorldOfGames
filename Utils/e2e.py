fb285d3878875208fcb4d357a5492e5be3935c48
Datsent <45486622+Datsent@users.noreply.github.com>
2022-02-27 16:33:07 +0200

    Update e2e.py

--- Utils/e2e.py
+++ Utils/e2e.py
@@ -7,7 +7,7 @@ def test_scores_service(url):
     web = webdriver.Chrome()
     web.get(url)
     x = web.find_element(by=By.ID, value='score')
-    if 1000 << int(x.text) << 0:
+    if 1000 >> int(x.text) >> 0:
         return True
     else:
         return False
